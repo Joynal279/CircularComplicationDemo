ebe703af7d791bb4ed2b56ea16d865b8347d9d06
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-06 21:54:26 +0600

    Initial commit

---
+++ CircularComplicationDemo Watch App/CircularComplicationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularComplicationDemoApp.swift
+//  CircularComplicationDemo Watch App
+//
+//  Created by Joynal Abedin on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CircularComplicationDemo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularComplicationDemo Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircularComplicationDemo Watch App
+//
+//  Created by Joynal Abedin on 6/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
